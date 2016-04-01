1e9389f7693c6e67223fe82e1dca2d653ea019aa
Gary Huang <gbhuang@cs.umass.edu>
2016-04-01 11:55:12 -0400

    updated compilation code to automatically cd into proper directory

--- fml_make.m
+++ fml_make.m
@@ -1,13 +1,17 @@
 function fml_make(single_threaded)
 % FML_MAKE(single_threaded) to compile flymatlib
-  
+
   if(exist('single_threaded','var') && ...
      single_threaded)
     mcc_out = '-o fml_dist_single -R singleCompThread';
   else
     mcc_out = '-o fml_dist';
   end
-  
+
+  fmlpath = fileparts(mfilename('fullpath'));
+  fprintf('changing directory to %s\n', fmlpath);
+  wd = cd(fmlpath);
+
   mcc_cmd = sprintf(...
     ['mcc -m -v %s -R -nodesktop -d fml_dist/ ' ...
      'utils/fml_dfeval_worker.m ' ...
@@ -15,5 +19,8 @@ function fml_make(single_threaded)
      '-a third_party/matconvnet ' ...
      '-a third_party/mexconv3d/mex_* ' ...
      '-a third_party/mexconv3d/util'], mcc_out);
+  fprintf('%s\n', mcc_cmd);
   eval(mcc_cmd);
+
+  cd(wd);
 end
