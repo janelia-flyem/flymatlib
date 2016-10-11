b08ebbf50a16c4f3f0e00183fa0f4acc00c2a762
Gary Huang <gbhuang@cs.umass.edu>
2016-10-11 13:26:12 -0400

    updated c3dae_infer to use addition context/buffer to avoid artifacts when stitching together results

--- cnn/c3dae_infer.m
+++ cnn/c3dae_infer.m
@@ -22,15 +22,22 @@ function out = c3dae_infer(net, image_fn, out_fn, ...
 
   out     = zeros(im_sz,'single');
 
-  yz_step  = 80;
+  yz_step  = 60;
+  yz_pad   = 10;
 
   net = fml_simplenn_move(net, 'gpu');
   for yy=1:yz_step:im_sz(2)
     y_end    = min(yy+yz_step-1,im_sz(2));
+    yp_l     = min(yz_pad, yy-1);
+    yp_u     = min(yz_pad, im_sz(2)-y_end);
+
     for zz = 1:yz_step:im_sz(3)
       z_end    = min(zz+yz_step-1,im_sz(3));
+      zp_l     = min(yz_pad, zz-1);
+      zp_u     = min(yz_pad, im_sz(3)-z_end);
 
-      im_slice = gpuArray(im(:,yy:y_end,zz:z_end));
+      im_slice = gpuArray(im(:,(yy-yp_l):(y_end+yp_u),...
+                               (zz-zp_l):(z_end+zp_u)));
       if(corrupt_type == 0)
         rr = randsample(im_prod, ...
                         floor(numel(im_slice)*corrupt_fraction));
@@ -42,8 +49,12 @@ function out = c3dae_infer(net, image_fn, out_fn, ...
       end
       res    = fml_simplenn(net,im_slice,[],[],'mode','test');
       res_sz = size(res(end).x);
-      out(1:res_sz(1),yy-1+(1:res_sz(2)),zz-1+(1:res_sz(3))) = ...
-          gather(res(end).x);
+
+      oo     = gather(res(end).x);
+      out(1:res_sz(1),yy-1+(1:(res_sz(2)-yp_u-yp_l)),...
+                      zz-1+(1:(res_sz(3)-zp_u-zp_l))) = ...
+          oo(:,(1+yp_l):(res_sz(2)-yp_u),...
+               (1+zp_l):(res_sz(3)-zp_u));
     end
   end
   toc
